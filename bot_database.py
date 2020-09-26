d2eefa288f253c598091392097d17dab4b80db8a
Maksim Pischulenok <maksim.pischulenok@outlook.com>
2020-09-26 21:10:18 +0300

    Add sqlite database support

---
+++ bot_database.py
@@ -0,0 +1,84 @@
+import sqlite3
+
+
+class BotDatabase:
+    def __init__(self, filename):
+        self.conn = sqlite3.connect(filename, check_same_thread=False)
+
+        self._add_users_table()
+        self._add_chats_table()
+
+    def add_user(self, user_id, username):
+        cursor = self.conn.cursor()
+        sql_query = '''INSERT OR IGNORE INTO users (user_id, username) VALUES (?, ?);'''
+        cursor.execute(sql_query, (user_id, username))
+        self.conn.commit()
+        cursor.close()
+
+    def get_all_users(self):
+        cursor = self.conn.cursor()
+        sql_query = '''SELECT user_id, username FROM users;'''
+        cursor.execute(sql_query)
+        records = cursor.fetchall()
+        cursor.close()
+        return records
+
+    def get_users_from_chat(self, group_id):
+        cursor = self.conn.cursor()
+        sql_query = '''SELECT c.user_id, u.username 
+                       FROM chats c 
+                       JOIN users u on c.user_id = u.user_id 
+                       WHERE c.chat_id=?;'''
+        cursor.execute(sql_query, (group_id,))
+        records = cursor.fetchall()
+        cursor.close()
+        return records
+
+    def add_user_to_chat(self, chat_id, user_id):
+        cursor = self.conn.cursor()
+        sql_query = '''INSERT OR IGNORE INTO chats (chat_id, user_id) VALUES (?, ?);'''
+        cursor.execute(sql_query, (chat_id, user_id))
+        self.conn.commit()
+        cursor.close()
+
+    def delete_user_from_chat(self, chat_id, user_id):
+        cursor = self.conn.cursor()
+        sql_query = '''DELETE from chats WHERE chat_id=? AND user_id=?;'''
+        cursor.execute(sql_query, (chat_id, user_id))
+        self.conn.commit()
+        cursor.close()
+
+    def update_user_username(self, user_id, new_username):
+        cursor = self.conn.cursor()
+        sql_update_query = '''UPDATE users SET username=? WHERE user_id=?;'''
+        cursor.execute(sql_update_query, (new_username, user_id))
+        self.conn.commit()
+        cursor.close()
+
+    def _add_users_table(self):
+        cursor = self.conn.cursor()
+        sql_query = '''CREATE TABLE IF NOT EXISTS 
+                                    users (user_id INT, username VARCHAR(32), PRIMARY KEY (user_id));'''
+        cursor.execute(sql_query)
+        self.conn.commit()
+        cursor.close()
+
+    def _add_chats_table(self):
+        cursor = self.conn.cursor()
+        sql_query = '''
+            CREATE TABLE IF NOT EXISTS chats (
+                chat_id INT, 
+                user_id INT, 
+                PRIMARY KEY (chat_id, user_id),
+                FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE );'''
+        cursor.execute(sql_query)
+        self.conn.commit()
+        cursor.close()
+
+    def close(self):
+        self.conn.close()
+
+
+if __name__ == '__main__':
+    db = BotDatabase('bot_users.db')
+    db.close()
--- main.py
+++ main.py
@@ -1,70 +1,52 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
-import sqlite3
+import logging
 
 import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+from bot_database import BotDatabase
 from constants import *
 
-import logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-chats = dict()
+db = BotDatabase('bot_users.db')
+
+
+def start_command(update, context):
+    message = 'Hey! I can help notify everyone in the group when someone needs them. \
+Everyone who wishes to receive mentions needs to /in to opt-in. All opted-in users can then be mentioned using /all'
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
 
 def in_command(update, context):
     chat_id = update.effective_chat.id
-
-    if not chat_id in chats:
-        chats[chat_id] = set()
-
     user = update.effective_user
     user_name = user.username or user.first_name or 'anonymous'
-
-    chats[chat_id].add((user.id, user_name))
+    db.add_user(user.id, user_name)
+    db.add_user_to_chat(chat_id, user.id)
     message = f'Thanks for opting in {user_name}'
-
     context.bot.send_message(chat_id=chat_id, text=message)
 
 
 def out_command(update, context):
     chat_id = update.effective_chat.id
-
-    if chat_id not in chats:
-        chats[chat_id] = set()
-
     user = update.effective_user
     user_name = user.username or user.first_name or 'anonymous'
-
-    if user in chats[chat_id]: chats[chat_id].remove(user)
-
+    db.delete_user_from_chat(chat_id, user.id)
     message = f'You\' ve been opted out {user_name}'
-
     context.bot.send_message(chat_id=chat_id, text=message)
 
 
 def all_command(update, context):
     chat_id = update.effective_chat.id
-
-    if (chat_id not in chats) or (not chats[chat_id]):
-        chats[chat_id] = set()
+    user_list = db.get_users_from_chat(chat_id)
+    if not user_list:
         message = 'There are no users\\. To opt in type /in command'
     else:
-        user_list = [f'[{user_name}](tg://user?id={user_id})' for user_id, user_name in chats[chat_id]]
+        user_list = [f'[{user_name}](tg://user?id={user_id})' for user_id, user_name in user_list]
         message = ', '.join(user_list)
-
     context.bot.send_message(chat_id=chat_id, text=message, parse_mode=telegram.ParseMode.MARKDOWN_V2)
 
-
-def test(update, context):
-    chat_id = update.effective_chat.id
-    user_id = update.effective_user.id
-    username = update.effective_user.username
-
-    message = f'[{username}](tg://user?id={user_id})'
-
-    context.bot.send_message(chat_id=chat_id, text=message,
-                             parse_mode=telegram.ParseMode.MARKDOWN_V2)
 
 def unknown_command(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
@@ -73,19 +55,17 @@ def unknown_command(update, context):
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
-
 in_handler = CommandHandler('in', in_command)
 all_handler = CommandHandler('all', all_command)
 out_handler = CommandHandler('out', out_command)
-test_handler = CommandHandler('test', test)
 unknown_command = MessageHandler(Filters.command, unknown_command)
 
 dispatcher.add_handler(in_handler)
 dispatcher.add_handler(all_handler)
 dispatcher.add_handler(out_handler)
-dispatcher.add_handler(test_handler)
 dispatcher.add_handler(unknown_command)
-
 
 updater.start_polling()
 updater.idle()
+
+db.close()
