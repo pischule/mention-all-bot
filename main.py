72d2ef33cfadc48d929bb001c56a62aab04daa0e
Maksim Pischulenok <maksim.pischulenok@outlook.com>
2020-10-03 00:07:02 +0300

    Revert "Make mentions invisible"
    This reverts commit 422bb5813077ca7380d246e2f05e9bd16023559d.
    
    Revert "Make mentions invisible"
    
    This reverts commit 39fd120cbffd353d4c7c7a3a58a3bdda0144e3dc.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram.utils.helpers import mention_markdown, escape_markdown
+from telegram.utils.helpers import mention_markdown
 
 from bot_database import BotDatabase
 from constants import *
@@ -14,9 +14,9 @@ db = BotDatabase('database.db')
 
 
 def start_command(update, context):
-    text = 'Hey! I can help notify everyone in the group when someone needs them. \
+    message = 'Hey! I can help notify everyone in the group when someone needs them. \
 Everyone who wishes to receive mentions needs to /in to opt-in. All opted-in users can then be mentioned using /all'
-    context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 
 def in_command(update, context):
@@ -25,8 +25,8 @@ def in_command(update, context):
     user_name = user.username or user.first_name or 'anonymous'
     db.add_user(user.id, user_name)
     db.add_user_to_chat(chat_id, user.id)
-    text = f'Thanks for opting in {user_name}'
-    context.bot.send_message(chat_id=chat_id, text=text)
+    message = f'Thanks for opting in {user_name}'
+    context.bot.send_message(chat_id=chat_id, text=message)
 
 
 def out_command(update, context):
@@ -38,25 +38,29 @@ def out_command(update, context):
     context.bot.send_message(chat_id=chat_id, text=message)
 
 
+def chunks(l, n):
+    n = max(1, n)
+    return (l[i:i + n] for i in range(0, len(l), n))
+
+
 def all_command(update, context):
     chat_id = update.effective_chat.id
     user_list = db.get_users_from_chat(chat_id)
-    message_id = update.message.message_id
-
     if not user_list:
-        text = escape_markdown('There are no users. To opt in type /in command', version=2)
+        message = 'There are no users. To opt in type /in command'
+        context.bot.send_message(chat_id=chat_id, text=message)
     else:
-        user_list = [mention_markdown(user_id, '\u2060', version=2) for user_id, user_name in user_list]
-        text = '🔔 Mentioned *all*' + ''.join(user_list)
-    context.bot.send_message(chat_id=chat_id, text=text, reply_to_message_id = message_id,
-                             parse_mode=telegram.ParseMode.MARKDOWN_V2)
+        user_list = [mention_markdown(user_id, user_name, version=2) for user_id, user_name in user_list]
+        for chunk in chunks(user_list, 4):
+            message = ', '.join(chunk)
+            context.bot.send_message(chat_id=chat_id, text=message, parse_mode=telegram.ParseMode.MARKDOWN_V2)
 
 
 def stats_command(update, context):
-    text = f'user_count: {db.count_users()[0]}\n' \
+    message = f'user_count: {db.count_users()[0]}\n' \
               f'chat_count: {db.count_chats()[0]}\n' \
               f'group_count: {db.count_groups()[0]}'
-    context.bot.send_message(update.effective_chat.id, text=text)
+    context.bot.send_message(update.effective_chat.id, text=message)
 
 
 def unknown_command(update, context):
